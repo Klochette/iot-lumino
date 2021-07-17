3c31ebb10443097fc0aefdb0247b9aec059c9a47
Klochette <melina.chamayou@hetic.net>
2021-07-17 18:25:13 +0200

    :hammer: delete dark mode

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,24 @@
 import React from "react";
 
-import { ThemeProvider } from "styled-components";
-
 import Router from "app/routes/Router";
 import style from "app/App.module.scss";
 import "scss/styles.scss";
-import { darkTheme, GlobalStyle, lightTheme } from "themes";
 import { SwitchTheme } from "features/switchTheme/SwitchTheme";
 import { useAppSelector } from "./store";
+import clsx from "clsx";
 
 const App = (): JSX.Element => {
-    const isLightMode = useAppSelector(
-        (state) => state.switchTheme.isLightMode
-    );
+    const { isDarkMode } = useAppSelector((state) => state.switchTheme);
+    console.log(style);
 
     return (
-        <ThemeProvider theme={isLightMode ? lightTheme : darkTheme}>
-            <GlobalStyle />
-            <h1 className={style.title}>Lumino</h1>
+        <>
+            <h1 className={clsx(style.title, isDarkMode && style.dark)}>
+                Lumino
+            </h1>
             <SwitchTheme />
             <Router />
-        </ThemeProvider>
+        </>
     );
 };
 
