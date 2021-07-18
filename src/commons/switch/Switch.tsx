dfee35c664760eaf5ca78d2f468cf01fb7b4f782
Klochette <melina.chamayou@hetic.net>
2021-07-18 17:37:24 +0200

    :construction: edit dark mode

--- src/commons/switch/Switch.tsx
+++ src/commons/switch/Switch.tsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 import style from "commons/switch/Switch.module.scss";
 import clsx from "clsx";
 
 type SwitchType = {
+    isChecked: boolean;
     onSwitch?: (e: React.MouseEvent<HTMLInputElement>) => void;
 };
 
-const Switch = ({ onSwitch }: SwitchType): JSX.Element => {
+const Switch = ({ onSwitch, isChecked }: SwitchType): JSX.Element => {
     return (
         <label className={style.switch}>
-            <input type="checkbox" onClick={onSwitch} />
+            <input type="checkbox" onClick={onSwitch} checked={isChecked} />
             <span className={clsx(style.slider, style.round)}></span>
         </label>
     );
