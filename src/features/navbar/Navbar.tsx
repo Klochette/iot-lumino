6f75d0d0fac135740ce48a538c49f4de9481def8
Klochette <melina.chamayou@hetic.net>
2021-07-19 13:16:34 +0200

    :hammer: delete unused variables

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -1,18 +1,13 @@
-import { useAppDispatch, useAppSelector } from "app/store";
-import SwitchTheme from "features/switchTheme/SwitchTheme";
 import React from "react";
+
 import styles from "./Navbar.module.scss";
-import clsx from "clsx";
-import { logout } from "features/user/userSlice";
-import { Link } from "react-router-dom";
+
 import { ReactComponent as Home } from "assets/images/bytesize_home.svg";
 import { ReactComponent as User } from "assets/images/carbon_user-filled.svg";
 import { ReactComponent as Alert } from "assets/images/fluent_alert-24-filled.svg";
 import { ReactComponent as Door } from "assets/images/fluent_conference-room-24-filled.svg";
 
 const Navbar = (): JSX.Element => {
-    const { isDarkMode } = useAppSelector((state) => state.switchTheme);
-
     return (
         <div className={styles.navbar}>
             <Home />
