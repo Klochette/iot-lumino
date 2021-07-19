d64e0ea299d590d9f7f0960ac2259e2d6402b37d
cpetillot <constance.petillot@hetic.net>
2021-07-19 18:59:34 +0200

    :flashlight: Dev page 404 not found

--- src/app/routes/notFound/NotFound.tsx
+++ src/app/routes/notFound/NotFound.tsx
@@ -1,8 +1,28 @@
 import React from "react";
 import styles from "./NotFound.module.scss";
+import { ReactComponent as LuminoLogo } from "assets/images/logoLumino.svg";
+import { ReactComponent as Four } from "assets/images/fourYB.svg";
 
 const NotFound = (): JSX.Element => {
-    return <div>NotFound</div>;
+    return (
+        <section>
+            <div className={styles.container}>
+                <div className={styles.images}>
+                    <Four className={styles.four} />
+                    <LuminoLogo className={styles.logoLumino} />
+                    <Four className={styles.four} />
+                </div>
+                <div className={styles.texts}>
+                    <p className={styles.text_bold}>
+                        Oh non ! Il n'y a rien ici
+                    </p>
+                    <p className={styles.text_normal}>
+                        Le renard a chipé la page
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default NotFound;
