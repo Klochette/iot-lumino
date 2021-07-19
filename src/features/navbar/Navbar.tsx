97de92d0d93f954175f8bf33e77961aa27aed51f
Klochette <melina.chamayou@hetic.net>
2021-07-19 15:53:34 +0200

    :memo: add routes

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -6,14 +6,26 @@ import { ReactComponent as Home } from "assets/images/bytesize_home.svg";
 import { ReactComponent as User } from "assets/images/carbon_user-filled.svg";
 import { ReactComponent as Alert } from "assets/images/fluent_alert-24-filled.svg";
 import { ReactComponent as Door } from "assets/images/fluent_conference-room-24-filled.svg";
+import { Link } from "react-router-dom";
+import { useAppSelector } from "app/store";
 
 const Navbar = (): JSX.Element => {
+    const { userType } = useAppSelector((state) => state.user);
+
     return (
         <div className={styles.navbar}>
-            <Home />
-            <Door />
-            <Alert />
-            <User />
+            <Link to={`/${userType}/dashboard`}>
+                <Home />
+            </Link>{" "}
+            <Link to={`/${userType}/rooms`}>
+                <Door />
+            </Link>{" "}
+            <Link to={`/${userType}/notifications`}>
+                <Alert />
+            </Link>{" "}
+            <Link to={`/${userType}/settings`}>
+                <User />
+            </Link>
         </div>
     );
 };
