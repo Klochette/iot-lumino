39261ada7b7a45e3e6e5bc372c5ec3bf96d6781f
cpetillot <constance.petillot@hetic.net>
2021-07-20 16:33:48 +0200

    beginning of the home page, header+firstcard, and change Bg notification

--- src/app/routes/dashboard/Dashboard.tsx
+++ src/app/routes/dashboard/Dashboard.tsx
@@ -1,8 +1,40 @@
 import React from "react";
 import styles from "./Dashboard.module.scss";
+import { ReactComponent as ProfilPicture } from "assets/images/profil_pic-nexus.svg";
+import { ReactComponent as CloseIcon } from "assets/images/close-circle.svg";
 
 const Dashboard = (): JSX.Element => {
-    return <div>dashboard</div>;
+    return (
+        <section className={styles.homeSection}>
+            <header className={styles.headerHome}>
+                <div className={styles.titleContainer}>
+                    <h1 className={styles.title}>
+                        <strong className={styles.titleBold}>Bonjour,</strong>
+                        <br />
+                        Gros bogosse
+                    </h1>
+                </div>
+                <ProfilPicture className={styles.profilPicture} />
+            </header>
+            <div className={styles.bookedRoom}>
+                <h2>Salle réservée</h2>
+                <div className={styles.containerBookedRoom}>
+                    <div className={styles.bookedRoomImg}>
+                        <p>A105</p>
+                        <CloseIcon className={styles.closeIcon} />
+                    </div>
+                    <div className={styles.rightSide}>
+                        <div className={styles.pictoGroupe}>
+                            <p>KTA</p>
+                        </div>
+                        <button className={styles.buttonGroupe}>
+                            Partager la salle au groupe
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Dashboard;
