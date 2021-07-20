bfca7664ea13e03fbe4d4092c1560e292a87431a
Hgo0123 <hcordillot@gmail.com>
2021-07-20 11:38:35 +0200

    profil page is ok

--- src/app/routes/settings/Settings.tsx
+++ src/app/routes/settings/Settings.tsx
@@ -3,8 +3,19 @@ import { useParams } from "react-router-dom";
 import styles from "./Settings.module.scss";
 import { ReactComponent as Pdp } from "assets/images/pdp.svg";
 import { useAppSelector } from "app/store";
+import Switch from "commons/switch/Switch";
+
 import { ReactComponent as LuminoLogo } from "assets/images/logoLumino.svg";
 import { ReactComponent as NexusLogo } from "assets/images/logoNexus.svg";
+import { ReactComponent as RightArrow } from "assets/images/right-arrow.svg";
+import { ReactComponent as MdpSvg } from "assets/images/mdp-svg.svg";
+import { ReactComponent as GroupSvg } from "assets/images/groups-svg.svg";
+import { ReactComponent as MobileSvg } from "assets/images/mobile-svg.svg";
+import { ReactComponent as EmailSvg } from "assets/images/email-svg.svg";
+import { ReactComponent as CGUSvg } from "assets/images/cgu-svg.svg";
+import { ReactComponent as ConfSvg } from "assets/images/config-svg.svg";
+
+
 
 const Settings = (): JSX.Element => {
     const { userType } = useParams<{ userType?: "student" | "admin" }>();
@@ -22,12 +33,59 @@ const Settings = (): JSX.Element => {
         </div>
         <div className = {styles.settings}>
             <div className={styles.account}>
-                <p>Compte</p>
-                <div className={styles.password}>Changer de mot de passe</div>
+                <p className={styles.account__title}>Compte</p>
+                <div className={styles.wrapper}>
+                    <div className={styles.wrapper__right}>
+                        <MdpSvg className={styles.wrapper__svg}/>
+                        <div className={styles.password}>Changer de mot de passe</div>
+                    </div>
+                    <RightArrow />
+                </div>
             </div>
-            <div className={styles.groups}></div>
-            <div className={styles.notifications}></div>
-            <div className={styles.preferences}></div>
+            <div className={styles.account}>
+                <p className={styles.account__title}>Groupe de travail</p>
+                <div className={styles.wrapper}>
+                    <div className={styles.wrapper__right}>
+                        <GroupSvg className={styles.wrapper__svg}/>
+                        <div className={styles.password}>Gérer mes groupes</div>
+                    </div>
+                    <RightArrow />
+                </div>
+            </div>
+            <div className={styles.account}>
+                <p className={styles.account__title}>Notifications</p>
+                <div className={styles.wrapper}>
+                    <div className={styles.wrapper__right}>
+                        <MobileSvg className={styles.wrapper__svg}/>
+                        <div className={styles.password}>Mobile</div>
+                    </div>
+                    <Switch />
+                </div>
+                <div className={styles.wrapper}>
+                    <div className={styles.wrapper__right}>
+                        <EmailSvg className={styles.wrapper__svg}/>
+                        <div className={styles.password}>Email</div>
+                    </div>
+                    <Switch />
+                </div>
+            </div>
+            <div className={styles.account}>
+                <p className={styles.account__title}>Préférences</p>
+                <div className={styles.wrapper}>
+                    <div className={styles.wrapper__right}>
+                        <CGUSvg className={styles.wrapper__svg}/>
+                        <div className={styles.password}>CGU</div>
+                    </div>
+                    <RightArrow />
+                </div>
+                <div className={styles.wrapper}>
+                    <div className={styles.wrapper__right}>
+                        <ConfSvg className={styles.wrapper__svg}/>
+                        <div className={styles.password}>Confidentialité</div>
+                    </div>
+                    <RightArrow />
+                </div>
+            </div>
             <div className={styles.disconnect}></div>
             <div className={styles.credits}>
                 <LuminoLogo className={styles.luminoLogo} />
