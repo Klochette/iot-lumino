291fb352e3b34952e836907c3d3d9a7999019f2a
Hgo0123 <hcordillot@gmail.com>
2021-07-21 12:47:26 +0200

    dev book page

--- src/app/routes/room/BookARoom.tsx
+++ src/app/routes/room/BookARoom.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import styles from "./Room.module.scss";
 import { useState } from "react";
 
 import clsx from "clsx";
+import { setUserError } from "features/user/userSlice";
 
 const BookARoom = (): JSX.Element => {
     const { userType } = useParams<{ userType?: "student" | "admin" }>();
@@ -24,6 +25,57 @@ const BookARoom = (): JSX.Element => {
     };
 
     const [isBooked, setIsBooked] = useState(bookingStore);
+
+    const [start, setStart] = useState<number|undefined>();
+    const [end, setEnd] = useState<number|undefined>();
+
+    const [error, setError] = useState<Boolean|undefined>(false);
+
+
+
+    const fixStartEnd = (int: any) => {       
+        //@ts-ignore
+        if (int > start || start !== undefined) {
+            setEnd(int)
+        }
+        //@ts-ignore
+        if(start === undefined || int < start){
+            setStart(int)
+        }
+
+        if(end === int){
+            for(const keys in isBooked){
+                //@ts-ignore
+                if(isBooked[keys] === 'userBooked' && keys > start && keys < end){
+                    //@ts-ignore
+                    setEnd(Number(keys))
+                }
+            }
+        }
+    }
+
+    useEffect(() =>  {
+
+        //@ts-ignore
+        for(const keys in isBooked){
+            //@ts-ignore
+            if(Number(keys) > start && Number(keys) < end && isBooked[keys] === 'free') {
+                //@ts-ignore
+                setIsBooked({...isBooked, [Number(keys)]: 'userBooked'})
+            }
+
+            //@ts-ignore
+            if(Number(keys) > start && Number(keys) < end && isBooked[keys] === 'booked') {
+                //@ts-ignore   
+                setError(true);
+            //@ts-ignore
+            } else if (Number(keys) > start && Number(keys) < end && isBooked[keys] !== 'booked'){
+                //@ts-ignore   
+                setError(false)
+            }
+        }
+    }, [isBooked])
+
     return (
         <section className={styles.section}>
             <div className={styles.topWrapper}>
@@ -36,76 +88,27 @@ const BookARoom = (): JSX.Element => {
                 </div>
             </div>
             <div className={styles.bottomWrapper}>
-                <p className={styles.bottomWrapper__start}>Arrivée</p>
+                {error &&
+                    <p className={styles.errorMsg}>Ce créneau horraires n'est pas disponible, veuillez en sélectionner un autre.</p>
+                }
+                <p className={styles.bottomWrapper__start}>{start === undefined ? "Séléctionnez votre départ" : "Séléctionnez votre arrivée"}</p>
                 <ul className={styles.bottomWrapper__list}>
-                    <li 
-                        className={clsx(styles.bottomWrapper__listItem, styles[isBooked[9]])}
-                        onClick={() => isBooked[9] === 'userBooked' ? setIsBooked({...isBooked, 9: 'free'}) :  setIsBooked({...isBooked, 9: 'userBooked'})
+                    {Object.keys(isBooked).map(function(key: any, index) {
+                       return <li 
+                        //@ts-ignore
+                        className={clsx(styles.bottomWrapper__listItem, styles[isBooked[key]])}
+                        onClick={() => {
+                            //@ts-ignore
+                            isBooked[key] === 'userBooked' ? setIsBooked({...isBooked, [key]: 'free'}) :  setIsBooked({...isBooked, [key]: 'userBooked'})
+                            fixStartEnd(key)
                         }
-                    >9:00</li>
-                    <li 
-                        className={clsx(styles.bottomWrapper__listItem, styles[isBooked[10]])}
-                        onClick={() => isBooked[10] === 'userBooked' ? setIsBooked({...isBooked, 10: 'free'}) :  setIsBooked({...isBooked, 10: 'userBooked'})}
-                    >10:00</li>
-                    <li 
-                        className={clsx(styles.bottomWrapper__listItem, styles[isBooked[11]])}
-                        onClick={() => isBooked[11] === 'userBooked' ? setIsBooked({...isBooked, 11: 'free'}) :  setIsBooked({...isBooked, 11: 'userBooked'})}
-
-                    >11:00</li>
-                    <li 
-                        className={clsx(styles.bottomWrapper__listItem, styles[isBooked[12]])}
-                        onClick={() => isBooked[12] === 'userBooked' ? setIsBooked({...isBooked, 12: 'free'}) :  setIsBooked({...isBooked, 12: 'userBooked'})}
-
-                    >12:00</li>
-                    <li 
-                        className={clsx(styles.bottomWrapper__listItem, styles[isBooked[13]])}
-                        onClick={() => isBooked[13] === 'userBooked' ? setIsBooked({...isBooked, 13: 'free'}) :  setIsBooked({...isBooked, 13: 'userBooked'})}
-
-                    >13:00</li>
-                    <li 
-                        className={clsx(styles.bottomWrapper__listItem, styles[isBooked[14]])}
-                        onClick={() => isBooked[14] === 'userBooked' ? setIsBooked({...isBooked, 14: 'free'}) :  setIsBooked({...isBooked, 14: 'userBooked'})}
-
-                    >14:00</li>
-                    <li 
-                        className={clsx(styles.bottomWrapper__listItem, styles[isBooked[15]])}
-                        onClick={() => isBooked[15] === 'userBooked' ? setIsBooked({...isBooked, 15: 'free'}) :  setIsBooked({...isBooked, 15: 'userBooked'})}
-
-                    >15:00</li>
-                    <li 
-                        className={clsx(styles.bottomWrapper__listItem, styles[isBooked[16]])}
-                        onClick={() => isBooked[16] === 'userBooked' ? setIsBooked({...isBooked, 16: 'free'}) :  setIsBooked({...isBooked, 16: 'userBooked'})}
-
-                    >16:00</li>
-                    <li 
-                        className={clsx(styles.bottomWrapper__listItem, styles[isBooked[17]])}
-                        onClick={() => isBooked[17] === 'userBooked' ? setIsBooked({...isBooked, 17: 'free'}) :  setIsBooked({...isBooked, 17: 'userBooked'})}
-
-                    >17:00</li>
-                    <li 
-                        className={clsx(styles.bottomWrapper__listItem, styles[isBooked[18]])}
-                        onClick={() => isBooked[18] === 'userBooked' ? setIsBooked({...isBooked, 18: 'free'}) :  setIsBooked({...isBooked, 18: 'userBooked'})}
-
-                    >18:00</li>
-                    <li 
-                        className={clsx(styles.bottomWrapper__listItem, styles[isBooked[19]])}
-                        onClick={() => isBooked[19] === 'userBooked' ? setIsBooked({...isBooked, 19: 'free'}) :  setIsBooked({...isBooked, 19: 'userBooked'})}
-
-                    >19:00</li>
-                    <li 
-                        className={clsx(styles.bottomWrapper__listItem, styles[isBooked[20]])}
-                        onClick={() => isBooked[20] === 'userBooked' ? setIsBooked({...isBooked, 20: 'free'}) :  setIsBooked({...isBooked, 20: 'userBooked'})}
-
-                    >20:00</li>
-                    <li 
-                        className={clsx(styles.bottomWrapper__listItem, styles[isBooked[21]])}
-                        onClick={() => isBooked[21] === 'userBooked' ? setIsBooked({...isBooked, 21: 'free'}) :  setIsBooked({...isBooked, 21: 'userBooked'})}
-
-                    >21:00</li>
+                        } 
+                    >{key}:00</li>
+                    })}
                 </ul>
+                <button className={styles.ctn}>Réservez</button>
             </div>
         </section>
     );
 };
-
 export default BookARoom;
